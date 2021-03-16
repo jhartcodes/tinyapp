e6349e8d6602c514a711020685a447f4f355da31
Joel <joel.d.hart1@gmail.com>
2021-03-16 16:44:12 +0000

    updated redirect to long url website for myurl page

--- express_server.js
+++ express_server.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+//global variables
+const generateRandomString = (num) =>  {
+  const char = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let random = '';
+  for (let i = 0; i < num ; i++) {
+    random += char[Math.floor(Math.random() * char.length)]    
+  };
+  return random;
+};
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 //ejs route handler for urls
 app.get("/urls", (req, res) => {
@@ -10,7 +26,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//addgetrote to show the form.
+app.post("/urls", (req, res) => {
+  const randomString = generateRandomString(6);
+  let longURL = 'http://'+req.body.longURL;
+  urlDatabase[randomString] = longURL;
+  let templateVars = {
+    shortURL: randomString, 
+    longURL: longURL
+  };
+  res.render('urls_show', templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.redirect(templateVars.longURL);
+});
+
+//set new route to submit a new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -20,13 +52,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// function generateRandomString() {
+//   let randomstr = string.generate(6)
+//   console.log(randomstr)
+//   return randomstr;
+// } could download random string packa and use.
 
