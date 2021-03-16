66ae21ad87a146e30621e64f2f58c469b3d74264
Joel <joel.d.hart1@gmail.com>
2021-03-16 00:56:21 +0000

    added newurl header

--- express_server.js
+++ express_server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 //ejs route handler for urls
 app.get("/urls", (req, res) => {
@@ -15,11 +10,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//addgetrote to show the form.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
